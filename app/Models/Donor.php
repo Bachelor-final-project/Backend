80ba7bdfe87b69eb83046ef5091cdd4539d58b2f
Ahmad Elhelou <ahmedmazenhelou2001@gmail.com>
2025-01-29 18:29:54 +0300

    add scopeForUser for some models in order to only show the authorized records for each user depending on their types

--- app/Models/Donor.php
+++ app/Models/Donor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\ForUserTrait;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,7 +11,7 @@ use App\Traits\TenantScoped;
 
 class Donor extends BaseModel
 {
-    use HasFactory, TenantAttributeTrait, TenantScoped;
+    use HasFactory, TenantAttributeTrait, TenantScoped, ForUserTrait;
     protected $appends = ['gender_str', 'country_name'];
     protected $guarded = ['donations'];
     protected $with = ['country'];
@@ -26,9 +27,20 @@ class Donor extends BaseModel
 
     ];
     public const PHONE_ALLOWED_USER_TYPES = [
-       1
+       1, 2
 
     ];
+    protected function phone(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => deterministicEncrypt($value),
+            get: function (string $value) {
+                if(in_array(auth()->user()->type, self::PHONE_ALLOWED_USER_TYPES))
+                    return deterministicDecrypt($value);
+                return str_repeat("●", 15);
+            },
+        );
+    }
 
     public static function genders() {
         
@@ -44,18 +56,18 @@ class Donor extends BaseModel
     {
         return $this->belongsTo(Country::class, 'country_id', 'id');
     }
-
-    protected function phone(): Attribute
+    public function proposals()
     {
-        return Attribute::make(
-            set: fn (string $value) => deterministicEncrypt($value),
-            get: function (string $value) {
-                if(in_array(auth()->user()->type, self::PHONE_ALLOWED_USER_TYPES))
-                    return deterministicDecrypt($value);
-                return str_repeat("●", 15);
-            },
+        return $this->hasManyThrough(
+            Proposal::class,     // Final related model
+            Donation::class,     // Intermediate model (pivot)
+            'donor_id',          // Foreign key on donations (to donors)
+            'id',                // Primary key on proposals
+            'id',                // Primary key on donors
+            'proposal_id'        // Foreign key on donations (to proposals)
         );
     }
+
 
     
     // public function getPhoneAttribute($phone)
@@ -72,6 +84,26 @@ class Donor extends BaseModel
     public function getGenderStrAttribute() {
         // dd($this);
         return [1 => __('Male'), 2 => __('Female'), 3 => ''][$this->gender ?? 3];
+    }
+
+    //scopes
+    public function scopeForUser($query, $user)
+    {
+        switch ($user->type) {
+            case 1: // proposal_director
+            case 3: // donations_director
+            case 4: // warehouses_director
+            case 5: // media_director
+                // access to all records
+                break;
+                
+            case 2: // entity_director
+                $query->has('proposals');
+                break;
+            default:
+                $query->whereRaw('1=0');
+                break;
+        }
     }
 
     public static function headers($user = null)
